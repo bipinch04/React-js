6cbef44fc9916864b081d4d59ba3ef78acadd42b
bipinch04 <bipinch04@gmail.com>
2026-02-02 12:08:32 +0545

    react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Hello react series</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello react with vite</h1>
+  )
+}
+
+export default App
